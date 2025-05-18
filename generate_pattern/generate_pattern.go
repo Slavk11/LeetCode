11e3370c1296ce551ed35024b9356f7cf8ecd7e2
Stanislav Sazonov <slavk11@yandex.ru>
2025-05-18 23:30:35 +0300

    Refactor for "Sum And Divide" Challenge Added "Generate Pattern" Challenge

---
+++ generate_pattern/generate_pattern.go
@@ -0,0 +1,21 @@
+package generate_pattern
+
+import "fmt"
+
+func GeneratePattern(n int) {
+	fmt.Println("🔧 Выполнение: Generate Pattern и Generate Pattern\n")
+
+	if n > 10 {
+		fmt.Print("The number should be less than 10")
+	}
+	for counter := n; counter > 0; counter-- {
+		fmt.Println(" ")
+		for i := 1; i <= counter; i++ {
+			fmt.Print(i)
+		}
+	}
+
+	fmt.Println("\n==============================")
+	fmt.Println("   ✅ Программа завершена    ")
+	fmt.Println("==============================")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"LeetCode/generate_pattern"
+	"LeetCode/sum_and_divide"
+)
+
+func main() {
+	sum_and_divide.SumAndDivide(4, 10)
+	generate_pattern.GeneratePattern(10)
+}
