e3d29a6fbaa72d4ffcfd4988372d1320ef6732f0
Stanislav Sazonov <slavk11@yandex.ru>
2025-05-27 00:32:10 +0300

    Refactor

--- slice_to_map/slice_to_map.go
+++ slice_to_map/slice_to_map.go
@@ -11,6 +11,10 @@ func SliceToMap(arr []string) map[string]int {
 	fmt.Printf("Inpit Array: %s\n", arr)
 	fmt.Println("Result Map:")
 	fmt.Print(testMap)
-	return testMap
 
+	fmt.Println("\n==============================")
+	fmt.Println("   ✅ Программа завершена    ")
+	fmt.Println("Алгоритмическая сложность по CPU - O(n), по памяти O(1)")
+	fmt.Println("==============================")
+	return testMap
 }
