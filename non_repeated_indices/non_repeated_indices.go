95dd349260d05b393bb41d2753b0731a44d96ae4
Stanislav Sazonov <slavk11@yandex.ru>
2025-06-01 20:57:37 +0300

    Refactor Non Repeated Indices

--- non_repeated_indices/non_repeated_indices.go
+++ non_repeated_indices/non_repeated_indices.go
@@ -21,7 +21,7 @@ func NonRepeatedIndices(arrStr []string) []int {
 	fmt.Printf("Output result: %d\n", result)
 	fmt.Println("\n==============================")
 	fmt.Println("   ✅ Программа завершена    ")
-	fmt.Println("Алгоритмическая сложность по CPU - O(n), по памяти O(1)")
+	fmt.Println("Алгоритмическая сложность по CPU - O(n), по памяти O(n)")
 	fmt.Println("==============================")
 
 	return result
