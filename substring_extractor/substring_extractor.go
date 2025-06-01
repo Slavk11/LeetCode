42765b2ccb87ff2dc83651ef55a44732245b9fa7
Stanislav Sazonov <slavk11@yandex.ru>
2025-06-01 20:28:07 +0300

    Refactor Substring Extractor

--- substring_extractor/substring_extractor.go
+++ substring_extractor/substring_extractor.go
@@ -4,19 +4,21 @@ import "fmt"
 
 func ExtractSubstring(word string, firstIndex int, lastIndex int) string {
 	var result string
+
 	if firstIndex == -1 || lastIndex == -1 {
 		return result
 	}
+
 	fmt.Print("🔧 Выполнение: Substring Extractor\n")
 	fmt.Printf("Input String: %s\nFirst Index: %d\nLast Index: %d\n", word, firstIndex, lastIndex)
 
-	for i := firstIndex; i <= lastIndex; i++ {
-		result += string(word[i])
-	}
+	result = word[firstIndex:lastIndex]
+
 	fmt.Printf("Output string %s\n", result)
 	fmt.Println("\n==============================")
 	fmt.Println("   ✅ Программа завершена    ")
 	fmt.Println("Алгоритмическая сложность по CPU - O(n), по памяти O(1)")
 	fmt.Println("==============================")
+
 	return result
 }
