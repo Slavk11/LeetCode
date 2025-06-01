10eca783bad6c43a65a4b796c644dcc18f950885
Stanislav Sazonov <slavk11@yandex.ru>
2025-06-01 20:50:19 +0300

    Refactor Slice To Map

--- slice_to_map/slice_to_map.go
+++ slice_to_map/slice_to_map.go
@@ -4,17 +4,17 @@ import "fmt"
 
 func SliceToMap(arr []string) map[string]int {
 	fmt.Println("🔧 Выполнение: Slice To Map\n")
-	testMap := make(map[string]int)
+
+	mapFromSlice := make(map[string]int, len(arr))
 	for _, str := range arr {
-		testMap[str] = len(str)
+		mapFromSlice[str] = len(str)
 	}
-	fmt.Printf("Inpit Array: %s\n", arr)
-	fmt.Println("Result Map:")
-	fmt.Print(testMap)
+	fmt.Printf("Input Array: %s\n", arr)
+	fmt.Println("Result Map:", mapFromSlice)
 
 	fmt.Println("\n==============================")
 	fmt.Println("   ✅ Программа завершена    ")
 	fmt.Println("Алгоритмическая сложность по CPU - O(n), по памяти O(1)")
 	fmt.Println("==============================")
-	return testMap
+	return mapFromSlice
 }
