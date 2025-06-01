da3c7a344a0e549eab0fae5dcfbbf0e36b57a112
Stanislav Sazonov <slavk11@yandex.ru>
2025-06-01 19:56:05 +0300

    Refactor Сhar Extractor

--- char_extractor/char_extractor.go
+++ char_extractor/char_extractor.go
@@ -7,21 +7,19 @@ func ExtractChar(word string, startChar string, lastChar string) string {
 	fmt.Printf("Input String: %s\nFirst Char: %s\nLast Char: %s\n", word, startChar, lastChar)
 
 	var result string
-	var first int
-	var last int
+	first := -1
+	last := -1
 
 	for i, char := range word {
-		if string(char) == startChar {
+		if string(char) == startChar && first == -1 {
 			first = i
 		}
-		if string(char) == lastChar {
+		if string(char) == lastChar && last == -1 {
 			last = i
 		}
 	}
 
-	for i := first; i <= last; i++ {
-		result = word[first : last+1]
-	}
+	result = word[first : last+1]
 
 	fmt.Printf("Output string is: %s", result)
 	fmt.Println("\n==============================")
