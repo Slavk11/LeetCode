1b5253c0a5c10ff9c7dd3a7b56ada5cd622fd8a1
Stanislav Sazonov <slavk11@yandex.ru>
2025-07-06 22:50:16 +0300

    Refactor Remove redundant space counting and correctly count

--- wordcounter/wordcounter.go
+++ wordcounter/wordcounter.go
@@ -3,7 +3,6 @@ package wordcounter
 import "fmt"
 
 func CountWords(sentence string) int {
-	const space byte = 32
 
 	if len(sentence) == 0 {
 		return 0
@@ -11,25 +10,17 @@ func CountWords(sentence string) int {
 
 	var counter int
 
-	for i := range len(sentence) {
-		if sentence[i] == space {
+	for i := 0; i < len(sentence); i++ {
+		if sentence[i] != ' ' && (i == 0 || sentence[i-1] == ' ') {
 			counter++
 		}
 	}
 
-	if sentence[0] == space {
-		counter--
-	}
-
-	if sentence[len(sentence)-1] == space {
-		counter--
-	}
-
-	fmt.Printf("В предложении: %s, %d: слова ", sentence, counter+1)
+	fmt.Printf("В предложении: %s, %d: слова ", sentence, counter)
 	fmt.Println("\n==============================")
 	fmt.Println("   ✅ Программа завершена    ")
 	fmt.Println("Алгоритмическая сложность по CPU - O(n), по памяти O(1)")
 	fmt.Println("==============================")
 
-	return counter + 1
+	return counter
 }
